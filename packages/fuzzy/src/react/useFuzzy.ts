b894a8e2e21ce47a26027f935b5d38c8e4038ea7
Pol Gubau Amores <gubaupol@gmail.com>
2025-03-26 21:12:16 +0100

    feat(react): ✨ add detailed documentation for useFuzzy options

--- packages/fuzzy/src/react/useFuzzy.ts
+++ packages/fuzzy/src/react/useFuzzy.ts
@@ -2,10 +2,34 @@ import React from "react";
 import { fuzzy, type Result } from "../index";
 
 export type useFuzzyOptions<T, U> = {
+	/**
+	 * List of items to search against
+	 */
 	list: T[];
+	/**
+	 * The query to search for. This is the string that will be searched for in the items.
+	 * @default ""
+	 * @example "foo"
+	 */
 	key?: string;
+	/**
+	 * Function that returns the string to search for in the item. This is useful if the item is an object and you want to search for a specific property.
+	 * @default undefined
+	 * @example (item) => [item.name]
+	 * @example (item) => [item.name, item.surname]
+	 */
 	getKey?: (item: T) => Array<string | null>;
+	/**
+	 * The query to search for. This is the string that will be searched for in the items.
+	 * @default ""
+	 * @example "foo"
+	 */
 	query: string;
+	/**
+	 * Function that maps the result item to a new item. This is useful if you want to transform the result item before returning it.
+	 * @default (result) => result.item
+	 * @example (result) => ({ ...result.item, score: result.score })
+	 */
 	mapResultItem: (result: Result<T>) => U;
 };
 
