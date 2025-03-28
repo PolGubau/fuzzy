7e1169e549b83b4a378f11090e7d0226b967369f
Pol Gubau Amores <gubaupol@gmail.com>
2025-03-28 14:42:31 +0100

    chore(landing): 🗑️ remove unused assets and configuration files

--- apps/landing/components/card.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-export const Card = (props: PropsWithChildren) => {
-	return (
-		<div className="flex flex-col gap-6 backdrop-blur-md p-4 md:p-6 rounded-3xl bg-gradient-to-b from-[#000000dd] to-transparent animate-blur">
-			{props.children}
-		</div>
-	);
-};
